cb8fcbe8650ebc0703d2f3e150b99571231720fe
Alexandros1992 <alex.tsieris@hotmail.com>
2026-02-01 12:39:21 +0200

    Started working in the front-end of the portal.

---
+++ app/page.jsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function Home() {
+    return (
+        <div className="min-h-screen bg-gray-50 p-10 text-center">
+            <h1 className="text-4xl font-serif text-purple-700 mb-4">Wedding Venue Booking</h1>
+            <p className="mb-6 text-purple-700">Check availability and request your date</p>
+
+            <Link className="bg-purple-600 text-white px-6 py-3 rounded-x1" href="/book"></Link>
+        </div>
+    );
+}
