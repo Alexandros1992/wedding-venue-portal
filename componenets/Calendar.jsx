cddab754aeaeddb54f6bcb122af23f5045d86cb6
Alexandros1992 <alex.tsieris@hotmail.com>
2026-02-02 23:33:52 +0200

    Started to build the components of the portal.

---
+++ componenets/Calendar.jsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "../lib/firebase";
+
+export default function Calendar({onSelect}) {
+    const [unavailable, setUnavailable] = useState([]);
+
+    useEffect(() => {
+        async function load() {
+            const snap = await getDocs(collection(db, "bookings"));
+
+            const dates = snap.docs.filter(d => d.data().status === "approved").map(d => d.data().date);
+            setUnavailable(dates);
+        }
+        load();
+    }, []);
+
+    return (
+        <input
+            type="date" className="border p-3 rounded w-full"
+            onChanage = {(e) => {
+                if (unavailable.includes(e.target.value)) {
+                    alert("This date is unavailable. Please select another date.");
+                    e.target.value = "";
+                } else {
+                    onSelect(e.target.value);
+                }   
+            }}
+            />
+    );
+}
